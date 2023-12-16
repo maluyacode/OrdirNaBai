0fc2a34f6ec1310602fa91dcde824376d85bc50e
maluyacode <116046282+maluyacode@users.noreply.github.com>
2023-12-16 09:05:24 +0800

    add-constant-variables

---
+++ backend/constants/PATH.js
@@ -0,0 +1,9 @@
+Object.freeze(
+    module.exports = {
+        USER_REGISTER: '/user/register',
+        USER_LOGIN: '/user/login',
+        USER_LOGOUT: '/user/logout',
+        USER_DELETE: '/user/delete/:id',
+        USER_UPDATE: '/user/update/:id'
+    }
+)
